096b8224ce8bd6a35c01936f165ef4b49d6b0cac
Deep Narayan Banerjee <147039086+Bearcry55@users.noreply.github.com>
2025-12-26 15:52:29 +0530

    add colors output config file

---
+++ colors.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	Reset  = "\033[0m"
+	Bold   = "\033[1m"
+
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Cyan   = "\033[36m"
+	White  = "\033[37m"
+	Gray   = "\033[90m"
+)
